a8f874063c0fc994ff8d8defda1c78d04c704a3a
Lachlan <mindcraftlachlan@outlook.com>
2021-04-29 00:53:39 +1000

    updated menu visuals
    * imported UI sprites
    * animated background to pan slightly
    * screen saver Timmy ft. flailing arms in spinning
    * minor foxes to anchor points so they don't mess up in different resolutions

---
+++ Assets/ScreenSaver.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSaver : MonoBehaviour
+{
+    Vector2 Direction;
+    public float speed = 1;
+    public float rotationSpeed = 0.2f;
+    float rotationdirection = 1;
+
+    float timer;
+    float extraSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Direction = new Vector2(1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        transform.Rotate(0, 0, speed * (rotationdirection + extraSpeed));
+        
+        
+
+        Vector2 bounds = new Vector2(Screen.width, Screen.height);
+        
+
+        if (transform.position.x < 30)
+        {
+            Direction.x = 1;
+            ToggleRotationDirection();
+        }
+
+        if (transform.position.y < 30)
+        {
+            Direction.y = 1;
+            ToggleRotationDirection();
+        }
+
+        if (transform.position.x > Screen.width-30)
+        {
+            Direction.x = -1;
+            ToggleRotationDirection();
+        }
+
+        if (transform.position.y > Screen.height-30)
+        {
+            Direction.y = -1;
+            ToggleRotationDirection();
+        }
+
+        if(extraSpeed > 0)
+        {
+            extraSpeed -= Time.deltaTime;
+        }
+        
+        transform.position = (Vector2)transform.position + (Direction * speed);
+    }
+
+    void ToggleRotationDirection()
+    {
+        rotationdirection *= -1f;
+
+        extraSpeed = 1 * rotationdirection;
+
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Animator animator;
-    
+    public Sprite buttonPressedSprite;
+    ParticleSystem buttonFeatherParticles;
+    Image imageComponent;
 
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Options()
+    public void Options(bool ToOptions)
     {
-        Debug.Log("Hey change that boolen");
-        animator.SetBool("ToOptions", true);
-
+        
+        animator.SetBool("ToOptions", ToOptions);
     }
+
+    public void spawnParticles(GameObject thisButton)
+    {
+        //thisButton.GetComponent<Image>().sprite = buttonPressedSprite;
+        //thisButton.GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+
 
 }
