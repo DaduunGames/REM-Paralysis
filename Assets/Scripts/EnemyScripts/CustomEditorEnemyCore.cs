870b0449a9be3bd24146a8a9b656a747210deea8
Lachlan <mindcraftlachlan@outlook.com>
2021-05-01 14:33:00 +1000

    optimized enemy animation
    setting up new animations no longer needed, just put the sprites into the inspector and it will do the logic in the background. animation is a bottleneck but this should help tonnes

---
+++ Assets/Scripts/EnemyScripts/CustomEditorEnemyCore.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(enemyCore))]
+[CanEditMultipleObjects]
+public class LookAtPointEditor : Editor
+{
+
+    SerializedProperty agroSpritesBool;
+    SerializedProperty agroDirectionBodies;
+
+    void OnEnable()
+    {
+
+        agroSpritesBool = serializedObject.FindProperty("hasAgroSprites");
+        agroDirectionBodies = serializedObject.FindProperty("agroDirectionalBodies");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+
+        foreach (string variable in enemyCore.AddToInspector)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(variable));
+        }
+
+
+
+        EditorGUILayout.PropertyField(agroSpritesBool);
+        if (agroSpritesBool.boolValue)
+        {
+            EditorGUILayout.PropertyField(agroDirectionBodies);
+        }
+
+
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/enemyCore.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+//[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AIPath))]
+
+public class enemyCore : MonoBehaviour
+{
+    // just in this script, if you want a variable to show up in inspect you have to:
+    // 1. set it as public
+    // 2. add it to the string list below
+
+    public static string[] AddToInspector = new string[] 
+    {
+        "health",
+        "SP",
+        "directionalBodies",
+        "IsAgro"
+    };
+
+
+    public float health;
+
+    public SpriteRenderer SP;
+
+    public Sprite[] directionalBodies;
+    public bool hasAgroSprites;
+    public Sprite[] agroDirectionalBodies;
+
+
+    private Animator anim;
+    private AIPath aiPath;
+
+
+    public bool IsAgro;
+
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        aiPath = GetComponent<AIPath>();
+    }
+
+    private void Update()
+    {
+        if (aiPath.velocity != Vector3.zero)    //When AI is Moving
+        {
+            anim.SetBool("Walking", true);
+        }
+        else                                    //Whenn AI is Still              
+        {
+            anim.SetBool("Walking", false);
+        }
+
+        int x = Mathf.RoundToInt(aiPath.velocity.normalized.x);
+        int y = Mathf.RoundToInt(aiPath.velocity.normalized.y);
+
+        switch(new Vector2(x, y))
+        {
+            case Vector2 v when v.Equals(new Vector2(0,1)):     //U
+                ChangeSprite(0);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(1, 1)):    //UR
+                ChangeSprite(1);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(1, 0)):    //R
+                ChangeSprite(2);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(1, -1)):   //DR
+                ChangeSprite(3);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(0, -1)):   //D
+                ChangeSprite(4);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(-1, -1)):  //DL
+                ChangeSprite(5);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(-1, 0)):   //L
+                ChangeSprite(6);
+                break;
+
+            case Vector2 v when v.Equals(new Vector2(-1, 1)):   //UL
+                ChangeSprite(7);
+                break;
+        }
+    }
+
+    private void ChangeSprite(int direction)
+    {
+        Sprite normalSprite = directionalBodies[direction];
+        Sprite agroSrite;
+
+        if (IsAgro) {
+            if (hasAgroSprites)
+            {
+                agroSrite = agroDirectionalBodies[direction];
+                SP.sprite = agroSrite;
+            }
+            else
+            {
+                SP.sprite = normalSprite;
+            }
+        }
+        else
+        {
+           
+            SP.sprite = normalSprite;
+            print($"setting sprite to {normalSprite.name}");
+        }
+    }
+}
