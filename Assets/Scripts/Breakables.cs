1f046ce789ed271bd6ecfe9dd40cc0e233c19f29
Kristina <kristinaverma01@gmail.com>
2021-05-05 16:00:33 +1000

    Breakables and Enemy
    Crate Placeholder
    Crate Particle System

---
+++ Assets/Scripts/Breakables.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakables : MonoBehaviour
+{
+    public GameObject particles;
+
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Yes");
+
+        if (col.gameObject.layer == 9)
+        {
+            Break();
+            Debug.Log("BeatYaYeet");
+        }
+    }
+
+
+    private void Break()
+    {
+        Instantiate(particles, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject shover;
+    public GameObject slinger;
+    public GameObject wizard;
+    Vector2 WhereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    public float spawnRadius;
+    bool canSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            WhereToSpawn = Random.insideUnitCircle * spawnRadius;
+           // WhereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(shover, WhereToSpawn, Quaternion.identity);
+            WhereToSpawn = Random.insideUnitCircle * spawnRadius;
+            Instantiate(slinger, WhereToSpawn, Quaternion.identity);
+            WhereToSpawn = Random.insideUnitCircle * spawnRadius;
+            Instantiate(wizard, WhereToSpawn, Quaternion.identity);
+        }
+
+    }
+}
